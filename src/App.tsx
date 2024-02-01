5faa9ec4e98e71f786b9931bc0bfe17c8f72d536
bruno <bg1821097@gmail.com>
2024-02-01 12:35:20 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
+import { RouterProvider } from 'react-router-dom'
+import { Button } from './components/ui/button'
+import './global.css'
+import { router } from './routes'
 
 export function App() { 
 
   return (
-    <>
-       hello wolrd
-    </>
+    <RouterProvider router={router}/>
   )
 }
  
